baaca492678b7f746dd9d6b29e0bb116fe2fd8f9
Geetesh Ranjan <geeteshranjan@gmail.com>
2025-08-10 15:49:17 +0530

    Update constants with Meta verification token

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,8 +1,9 @@
 export const PRODUCT_BRAND_NAME = "adctopus";
 export const COMPANY_LEGAL_NAME = "adctopus";
-export const DOMAIN = "DOMAIN"; // e.g., https://yourdomain.com
+export const DOMAIN = "https://www.adctopus.com"; // full URL with www because Vercel set www as primary
 export const SUPPORT_EMAIL = "geetesh17r@gmail.com";
 export const BUSINESS_ADDRESS = `S/O: Anil Kumar Chaurasia, VILLAGE BARIGORA, Jojobera, East Singhbhum, Rahargora, Jharkhand, 831016`;
 export const COUNTRY_JURISDICTION = "India";
-export const META_DOMAIN_VERIFICATION = "REPLACE_WITH_META_TOKEN";
+export const META_DOMAIN_VERIFICATION = "g4yngfrppif53rv6zgk8p0fcn15cv5"; // your Meta token
 export const PHONE = "9334721858";
+
